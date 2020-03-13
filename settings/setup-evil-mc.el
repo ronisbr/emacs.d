0249ab8707ce363f61f25cec3717cdd3a0045b24
Ronan Arraes Jardim Chagas <ronisbr@gmail.com>
2020-03-13 16:13:17 -0300

    :wrench: Add Julia LaTeX sub to MC known commands

--- settings/setup-evil-mc.el
+++ settings/setup-evil-mc.el
@@ -8,10 +8,17 @@
 (use-package evil-mc
   :after evil
   :init
+  ;; Add known commands.
   (global-evil-mc-mode t)
 
   :config
   ;; When ESC is pressed in normal mode, then destroy the multiple cursors.
-  (evil-define-key 'normal evil-mc-key-map (kbd "<escape>") 'evil-mc-undo-all-cursors))
+  (evil-define-key 'normal evil-mc-key-map (kbd "<escape>") 'evil-mc-undo-all-cursors)
+
+  ;; Add supported commands to multiple cursors.
+  (nconc evil-mc-known-commands
+         '((julia-latexsub-or-indent . ((:default . evil-mc-execute-default-call-with-count))))))
+
+
 
 (provide 'setup-evil-mc)
