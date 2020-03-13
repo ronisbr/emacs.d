25032e0d02843667813b5b78daa589bfb89066c1
Ronan Arraes Jardim Chagas <ronisbr@gmail.com>
2020-03-13 13:23:31 -0300

    :wrench: Improve commands split and vsplit
    Those now behave as in Vim with the options `splitbelow` and
    `splitright`.

--- settings/setup-evil-mode.el
+++ settings/setup-evil-mode.el
@@ -9,6 +9,27 @@
   :init
   (setq evil-respect-visual-line-mode t)
   :config
-  (evil-mode 1))
+  (evil-mode 1)
+
+  ;; Ex commands.
+  (evil-ex-define-cmd "sp[lit]"  'split-window-below-and-focus)
+  (evil-ex-define-cmd "vs[plit]" 'split-window-right-and-focus))
+
+;; Functions for the Ex commands.
+(evil-define-command split-window-right-and-focus (arg)
+  "Split the window vertically and focus the new window."
+  (interactive "<a>")
+  (split-window-right)
+  (windmove-right)
+  (when arg
+    (evil-edit arg)))
+
+(evil-define-command split-window-below-and-focus (arg)
+  "Split the window horizontally and focus the new window."
+  (interactive "<a>")
+  (split-window-below)
+  (windmove-down)
+  (when arg
+    (evil-edit arg)))
 
 (provide 'setup-evil-mode)
