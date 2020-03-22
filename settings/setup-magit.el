c2f6836314af800bca130c5e21f0f0aa0810c4c9
Ronan Arraes Jardim Chagas <ronisbr@gmail.com>
2020-03-22 11:45:00 -0300

    :wrench: Set fill-column to 67 in magit commit message

--- settings/setup-magit.el
+++ settings/setup-magit.el
@@ -6,6 +6,8 @@
 ;; =============================================================================
 
 (use-package magit
-  :commands (magint magit-status))
+  :commands (magint magit-status)
+  :config
+  (add-hook 'git-commit-mode-hook (lambda () (setq fill-column 67))))
 
 (provide 'setup-magit)
