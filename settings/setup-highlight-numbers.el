d78cb5611a08aa56b0d83983a218c6ca258d904e
Ronan Arraes Jardim Chagas <ronisbr@gmail.com>
2020-06-13 16:35:55 -0300

    :wrench: Fix number highlighting in Julia mode

--- settings/setup-highlight-numbers.el
+++ settings/setup-highlight-numbers.el
@@ -6,6 +6,29 @@
 ;; =============================================================================
 
 (use-package highlight-numbers
-  :hook (prog-mode . highlight-numbers-mode))
+  :hook (prog-mode . highlight-numbers-mode)
+  :config
+  ;; Fix number highlighting in Julia mode.
+  (puthash 'julia-mode
+           (rx (and
+                symbol-start
+                (or
+                 ;; Match hexadecimal number.
+                 (and "0x"
+                      (+ (and hex-digit (? "_"))))
+                 ;; Match octal number.
+                 (and "0o"
+                      (+ (and (any "012345678") (? "_"))))
+                 ;; Match binary numbers.
+                 (and "0b"
+                      (+ (and (any "01") (? "_"))))
+                 ;; Match other numbers.
+                 (and (+ (and digit (? "_")))
+                      (? (and "." (* (and digit (? "_")))))
+                      (? (and (any "eE")
+                              (? (any "-+"))
+                              (+ (and digit (? "_")))))))
+                symbol-end))
+           highlight-numbers-modelist))
 
 (provide 'setup-highlight-numbers)
